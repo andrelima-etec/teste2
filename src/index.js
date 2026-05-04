92fcfb75741769d7cfdeefe131a398b4b074d89d
André Lima <andre.lima190.etec@gmail.com>
2026-05-04 20:40:42 -0300

    Chamadas HTTP via REST Client

--- src/index.js
+++ src/index.js
@@ -4,13 +4,35 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send("Olá, mundo!!!");
+    // res.send("Olá, mundo!!!");
+    res.json({ message: "Olá, mundo!" });
 });
 
 app.get('/welcome', (req, res) => {
     const name = req.query.nome || 'visitante';
-    res.send(`Olá, ${name}`);
+    // res.send(`Olá, ${name}`);
+    res.json({ message: `Olá, ${name}`});
+});
+
+app.get('/dobro/:numero', (req, res) => {
+    const numero = req.params.numero;
+
+    const dobro = numero * 2;
+
+    // res.send(`O dobro de ${numero} é ${dobro}.`);
+    res.json({ numero, dobro});
+});
+
+app.post('/operacoes/somar', (req, res) => {
+    const numero1 = 1;
+    const numero2 = 2;
+
+    const resultado = numero1 + numero2;
+
+    res.json({ result: resultado });
 });
 
 const PORTA = process.env.PORT || 3000;
